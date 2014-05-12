c6d71de35c9d24cc34c0fdc3ab1e93f054b6a71c
MisterWeasel <heerkip@gmail.com>
2014-05-12 17:24:01 +0200

    Additional users for testing

--- UserGenerator.java
+++ UserGenerator.java
@@ -16,21 +16,20 @@ public class UserGenerator {
 	public static void main(String[] args) {
 		try {
 			UserData me = new UserData();
-			addPurchase(me, 200, 110, 2014, 1, 2);
-			addPurchase(me, 230, 333, 2011, 3, 18);
-			addPurchase(me, 20, 131, 2012, 4, 26);
-			addPurchase(me, 100, 99, 2019, 9, 4);
-			addPurchase(me, 900, 374, 2000, 7, 3);
-			addPurchase(me, 250, 34, 2003, 2, 6);
-			addPurchase(me, 90, 86, 2020, 11, 11);
-			addPurchase(me, 330, 67, 2010, 0, 13);
-			addPurchase(me, 299, 123, 2012, 8, 15);
-			addPurchase(me, 223, 98, 2011, 7, 27);
-			addPurchase(me, 145, 36, 2013, 4, 19);
-			addPurchase(me, 123, 200, 2014, 3, 3);
-			addPurchase(me, 29, 1, 2014, 0, 29);
+			addPurchase(me, 200, 401, 2014, 1, 2);
+			addPurchase(me, 23, 333, 2011, 3, 18);
+			addPurchase(me, 520, 603, 2012, 4, 26);
+			addPurchase(me, 10, 30, 2007, 9, 4);
+			addPurchase(me, 90, 37, 2000, 7, 3);
+			addPurchase(me, 25, 50, 2003, 2, 6);
+			addPurchase(me, 190, 40, 2002, 11, 11);
+			addPurchase(me, 33, 20, 2010, 0, 13);
+			addPurchase(me, 29, 350, 2012, 8, 15);
+			addPurchase(me, 23, 302, 2011, 7, 27);
+			addPurchase(me, 15, 360, 2013, 4, 19);
+			addPurchase(me, 12, 153, 2014, 3, 3);
 
-			saveUserToFile(me, "PiratPelle", "sudd");
+			saveUserToFile(me, "Simple", "Mordor");
 		} catch(IOException e) {
 			e.printStackTrace();
 		}
